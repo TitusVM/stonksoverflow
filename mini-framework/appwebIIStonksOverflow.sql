6b4aa1e229c93d55f43d54d54dd4293f947944fe
Moreira Miguel <miguel.moreira@he-arc.ch>
2022-03-30 09:07:45 +0200

    updating sql file

--- mini-framework/appwebIIStonksOverflow.sql
+++ mini-framework/appwebIIStonksOverflow.sql
@@ -1,12 +1,12 @@
-DROP DATABASE IF EXISTS appWeb2StonksOverflow;
-CREATE DATABASE appWeb2StonksOverflow
+DROP DATABASE IF EXISTS appWebIIStonksOverflow;
+CREATE DATABASE appWebIIStonksOverflow
   CHARACTER SET 'utf8mb4'
   COLLATE 'utf8mb4_unicode_ci';
 
-USE appWeb2StonksOverflow;
+USE appWebIIStonksOverflow;
 
 CREATE TABLE Users(
-   id INT,
+   id INT AUTO_INCREMENT,
    username VARCHAR(50) NOT NULL,
    passwd VARCHAR(50) NOT NULL,
    PRIMARY KEY(id),
@@ -14,7 +14,7 @@ CREATE TABLE Users(
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE Questions(
-   id INT,
+   id INT AUTO_INCREMENT,
    mainText TEXT,
    datetimestamp DATETIME NOT NULL,
    idUser INT NOT NULL,
@@ -23,10 +23,10 @@ CREATE TABLE Questions(
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE Answers(
-   id INT,
+   id INT AUTO_INCREMENT,
    mainText TEXT,
    datetimestamp DATETIME NOT NULL,
-   solution LOGICAL NOT NULL,
+   solution TINYINT NOT NULL,
    idQuestion INT NOT NULL,
    idUser INT NOT NULL,
    PRIMARY KEY(id),
@@ -35,14 +35,14 @@ CREATE TABLE Answers(
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE Comments(
-   id INT,
+   id INT AUTO_INCREMENT,
    mainText TEXT,
    datetimestamp DATETIME NOT NULL,
    idUser INT NOT NULL,
-   idAnswer INT NOT NULL,
-   idQuestion INT NOT NULL,
+   idAnswer INT,
+   idQuestion INT,
    PRIMARY KEY(id),
-   FOREIGN KEY(idUsers) REFERENCES Users(id),
+   FOREIGN KEY(idUser) REFERENCES Users(id),
    FOREIGN KEY(idAnswer) REFERENCES Answers(id),
    FOREIGN KEY(idQuestion) REFERENCES Questions(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -52,18 +52,20 @@ ALTER TABLE Questions AUTO_INCREMENT = 1;
 ALTER TABLE Answers AUTO_INCREMENT = 1;
 ALTER TABLE Comments AUTO_INCREMENT = 1;
 
-INSERT INTO Comments(mainText, datetimestamp, idUser, idAnswer, idQuestion)
-   VALUES ('Ceci est un commentaire qui répond à une question', now(), 1, 0, 1),
-          ('Ceci est un commentaire qui répond à une réponse', now(), 1, 1, 0),
-          ('Ceci est un commentaire qui répond à un commentaire', now(), 2, 1, 0);
+INSERT INTO Users(username, passwd)
+   VALUES ('test', 'test'),
+          ('test2', 'test2');
+
+INSERT INTO Questions(mainText, datetimestamp, idUser)
+   VALUES ('Ceci est une question', now(), 1),
+          ('Ceci est une autre question', now(), 2);
 
 INSERT INTO Answers(mainText, datetimestamp, solution,  idQuestion, idUser)
-   VALUES ('Ceci est une réponse qui répond à une question', now(), 0, 1, 1),
-          ('Ceci est une réponse qui répond à une réponse', now(), 1, 1, 2);
+   VALUES ('Ceci est une réponse qui répond à une question', now(), 1, 1, 1),
+          ('Ceci est une autre réponse qui répond à une question', now(), 0, 1, 2),
+          ('Ceci est une autre réponse qui répond à une autre question', now(), 1, 2, 2);
 
-INSERT INTO Questions(mainText, datetimestamp, idQuestion, idUser)
-   VALUES ('Ceci est une réponse qui répond à une question', now(), 1, 1),
-          ('Ceci est une réponse qui répond à une réponse', now(), 1, 2);
-
-INSERT INTO Users(mainText, datetimestamp, idUser)
-   VALUES ('Ceci est une question', now(), 1);
+INSERT INTO Comments(mainText, datetimestamp, idUser, idAnswer, idQuestion)
+   VALUES ('Ceci est un commentaire qui répond à une question', now(), 1, NULL, 1),
+          ('Ceci est un commentaire qui répond à une réponse', now(), 1, 1, NULL),
+          ('Ceci est un autre commentaire qui répond à une réponse', now(), 2, 1, NULL);
